e3510792b4e13a9deac2f43a42936f5498f44ff8
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-06-09 13:43:21 +0300

    Update app.js

--- homeworkSixth/3/app.js
+++ homeworkSixth/3/app.js
@@ -8,21 +8,19 @@
 // </body>
 //1 спосіб
 
-// document.body.innerHTML = `<main class="mainClass check item">
-//     <div id="myDiv">
-//         <p>First paragraph</p>
-// </div>
-//  </main>`;
+document.body.innerHTML = `<main class="mainClass check item">
+    <div id="myDiv">
+        <p>First paragraph</p>
+</div>
+ </main>`;
 
 //2 спосіб
 
 document.body.innerHTML = "";
-
 const main = document.createElement("main");
 main.className = "mainClass check item";
 const div = document.createElement("div");
 div.id = "myDiv";
 main.append(div);
 div.innerHTML = "<p>First paragraph</p>";
-
 document.body.appendChild(main);
